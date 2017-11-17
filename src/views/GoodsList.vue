63033050d3b2bee1a9235ade948bfbb52d10a0f9
hf2020520 <1968292743@qq.com>
2017-11-17 15:28:40 +0800

    实现购物成功弹框

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -59,6 +59,18 @@
         <a class="btn btn--m" href="javascript:void(0);" @click="mdShow = false">关闭</a>
       </div>
     </modal>
+    <modal :mdShow="mdShowCart" v-on:close="closeModal">
+      <p slot="message">
+        <svg class="icon-status-ok">
+          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-status-ok"></use>
+        </svg>
+        <span>加入购物车成功!</span>
+      </p>
+      <div slot="btnGroup">
+        <a class="btn btn--m" href="javascript:void(0);" @click="mdShowCart = false">继续购物</a>
+        <router-link class="btn btn--m btn--red" href="javascript:;" to="/cart">查看购物车</router-link>
+      </div>
+    </modal>
     <div class="md-overlay" v-show="overLayFlag" @click.stop="closePop"></div>
     <nav-footer></nav-footer>
     </body>
@@ -112,7 +124,8 @@
         pageSize: 8,
         busy: true,
         loading: false,
-        mdShow: false
+        mdShow: false,
+        mdShowCart: false
       }
     },
     mounted () {
@@ -169,6 +182,7 @@
       closePop () {
         this.filterBy = false
         this.overLayFlag = false
+        this.mdShowCart = false
       },
       defaultSort () {
         this.sortFlag = true
@@ -194,6 +208,7 @@
           res = res.data
           if (res.status === '0') {
             /* alert('加入成功') */
+            this.mdShowCart = true
           } else {
             /* alert('Error msg:' + res.msg) */
             this.mdShow = true
@@ -202,6 +217,7 @@
       },
       closeModal () {
         this.mdShow = false
+        this.mdShowCart = false
       }
     }
   }
