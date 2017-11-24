b2e949ac26412cdf73b1a202aae143a05dec3b99
hf2020520 <1968292743@qq.com>
2017-11-24 10:57:44 +0800

    引入vuex

--- src/main.js
+++ src/main.js
@@ -5,14 +5,34 @@ import App from './App'
 import router from './router'
 import VueLazyload from 'vue-lazyload'
 import infiniteScroll from 'vue-infinite-scroll'
+import axios from 'axios'
+import Vuex from 'vuex'
 
 Vue.use(infiniteScroll)
+Vue.use(Vuex)
 Vue.use(VueLazyload, {
   loading: 'static/loading-svg/loading-bars.svg',
   attempt: 3 // default 1
 })
 
 Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+  state: {
+    nickName: '',
+    cartCount: 0
+  },
+  mutation: {
+    // 更新用户信息
+    updateUserInfo (state, nickName) {
+      state.nickName = nickName
+    },
+    // 更新购物车信息
+    updateCartCount (state, cartCount) {
+      state.cartCount += cartCount
+    }
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
